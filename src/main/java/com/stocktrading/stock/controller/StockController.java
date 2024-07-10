c88bd3e6089f3fd060658a85e2021a6b0b65f0a6
Choi Seongwon <vlflalektzja@naver.com>
2024-07-10 18:43:32 +0900

    feat: 탑 100 주식 정보 초기화, 불러오기 구현 완료

--- src/main/java/com/stocktrading/stock/controller/StockController.java
+++ src/main/java/com/stocktrading/stock/controller/StockController.java
@@ -1,11 +1,19 @@
 package com.stocktrading.stock.controller;
 
+import com.stocktrading.stock.controller.dto.GetTop100StocksResponse;
+import com.stocktrading.stock.controller.dto.StockInfo;
 import com.stocktrading.stock.service.StockService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stocks")
@@ -21,6 +29,18 @@ public class StockController {
     public ResponseEntity<Void> getStockInfo(@PathVariable String stockTicker) {
         stockService.findStock(stockTicker);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/top100")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void initTop100Stocks(@RequestBody List<StockInfo> stockInfos) {
+        stockService.initTop100(stockInfos);
+    }
+
+    @GetMapping("/top100")
+    public GetTop100StocksResponse getTop100Stocks() {
+        List<StockInfo> stockInfos = stockService.getTop100();
+        return new GetTop100StocksResponse(stockInfos);
     }
 
 }
--- src/main/java/com/stocktrading/stock/domain/Stock.java
+++ src/main/java/com/stocktrading/stock/domain/Stock.java
@@ -1,18 +1,24 @@
 package com.stocktrading.stock.domain;
 
 import com.stocktrading.global.BaseTimeEntity;
+import com.stocktrading.stock.controller.dto.StockInfo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = false)
 public class Stock extends BaseTimeEntity {
 
     @Id
+    @Getter
     @GeneratedValue
     private Long id; // 순위
 
@@ -20,8 +26,29 @@ public class Stock extends BaseTimeEntity {
 
     private String name;
 
-    private Long price;
+    private Double price;
 
-    private String imageUrl;
+    private Float today;
+
+    private String logoUrl;
+
+    @Builder
+    public Stock(String stockTicker, String name, Double price, Float today, String logoUrl) {
+        this.stockTicker = stockTicker;
+        this.name = name;
+        this.price = price;
+        this.today = today;
+        this.logoUrl = logoUrl;
+    }
+
+    public StockInfo toDto() {
+        return new StockInfo(
+                name,
+                stockTicker,
+                logoUrl,
+                "$" + price,
+                today >= 0 ? "+" + today + "%" : today + "%"
+        );
+    }
 
 }
