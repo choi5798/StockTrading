b262cada8ae43d6daf9c66b867bf0ecbe162ff32
Choi Seongwon <vlflalektzja@naver.com>
2024-07-11 15:59:44 +0900

    feat: response status 변경

--- src/main/java/com/stocktrading/member/controller/MemberStockController.java
+++ src/main/java/com/stocktrading/member/controller/MemberStockController.java
@@ -6,11 +6,13 @@ import com.stocktrading.member.service.MemberStockService;
 import com.stocktrading.stock.controller.dto.StockInfo;
 import com.stocktrading.stock.domain.Stock;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,6 +31,7 @@ public class MemberStockController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void bookmark(@RequestParam("ticker") String stockTicker, HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization").substring(7);
         String username = jwtProvider.getUsername(bearerToken);
@@ -36,6 +39,7 @@ public class MemberStockController {
     }
 
     @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unbookmark(@RequestParam("ticker") String stockTicker, HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization").substring(7);
         String username = jwtProvider.getUsername(bearerToken);
